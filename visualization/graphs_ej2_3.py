62be0224c9dda63a3ec1847580ebb16da8f8c04b
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-05-08 23:34:18 -0300

    finished ej2.3

--- visualization/graphs_ej2_3.py
+++ visualization/graphs_ej2_3.py
@@ -1,1 +1,25 @@
 import matplotlib.pyplot as plt
+
+f = open("../data/electric/trajectoryVaryingSpeed.txt")
+
+line = f.readline().strip()
+
+vels = []
+trajectories = []
+
+while line and line != "":
+    line = list(map(float,line.split(":")))
+    
+    vels.append(line[0])
+    trajectories.append(line[1])
+
+    line = f.readline().strip()
+
+plt.gcf().set_size_inches(16, 12)
+plt.xlabel("velocidades",fontsize=15)
+plt.ylabel("trayectorias",fontsize=15)
+
+plt.scatter(vels,trajectories)
+plt.plot(vels,trajectories)
+
+plt.show()
